9bad2ea83b40eea61000d7e1b46b8593da8c2003
bentrend <trend4309@gmail.com>
2024-01-05 11:09:43 +0300

    feat: compleat 7 passing

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -251,8 +251,16 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let revStr = '';
+  for (let i = str.length - 1; i >= 0; ) {
+    revStr += str[i];
+    i -= 1;
+  }
+  if (revStr === str) {
+    return true;
+  }
+  return false;
 }
 
 /**
